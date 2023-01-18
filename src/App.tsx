06d901d2a8167feb6a6b1c08b7e6964204d904c4
diiimmmaaas <dm.sokolov1994@gmail.com>
2023-01-18 13:43:53 +0300

    add eslint and prettier to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div>
-      Hello administrator (adygeya)
-    </div>
-  );
+  return <div>Hello administrator (adygeya)</div>
 }
 
-export default App;
+export default App
