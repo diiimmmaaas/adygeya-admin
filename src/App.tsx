101e7c81a46956898e9c0f04a7874ad46b6b3865
diiimmmaaas <dm.sokolov1994@gmail.com>
2023-01-19 17:20:07 +0300

    create Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 import RouteList from './navigation/RouteList'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div role='presentation' className='App'>
+      <Navbar />
       <RouteList />
     </div>
   )
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import styles from './Navbar.module.css'
+import logo from '../../assets/img/logo.png'
+import { useNavigate } from 'react-router-dom'
+import { PATH } from '../../navigation/path'
+
+const buttons = [
+  { id: 1, title: 'Объекты', path: PATH.objectCardPage },
+  { id: 2, title: 'События', path: PATH.newsCardPage },
+  { id: 3, title: 'Маршруты', path: PATH.routeCardPage },
+  { id: 4, title: 'Пользователи', path: PATH.usersPage },
+  { id: 5, title: 'Уведомления', path: PATH.notificationsPage },
+  { id: 6, title: 'Настройки', path: PATH.settingsPage },
+]
+
+const Navbar = () => {
+  const [activeBtn, setActiveBtn] = useState(1)
+
+  const navigate = useNavigate()
+
+  const isAuth = true
+
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.logoContainer}>
+            <img className={styles.logo} src={logo} alt='logo' onClick={() => navigate('/')} />
+          </div>
+          {isAuth ? (
+            <div className={styles.functionalityContainer}>
+              <ul className={styles.buttonsList}>
+                {buttons.map((btn) => {
+                  const onClickBtn = () => {
+                    navigate(btn.path)
+                    setActiveBtn(btn.id)
+                  }
+                  return (
+                    <li
+                      key={btn.id}
+                      onClick={onClickBtn}
+                      className={activeBtn === btn.id ? styles.activeBtn : styles.btn}
+                    >
+                      {btn.title}
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          ) : (
+            <div className={styles.loginContainer}>
+              <p className={styles.loginText}>Log In</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
